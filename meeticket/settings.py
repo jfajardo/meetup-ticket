92b59b63aca8d8e9ad96d29dd4d8cca86cde5092
Jonhatan Fajardo <jfajardo89@gmail.com>
2018-10-11 14:19:22 -0500

    debug

--- meeticket/settings.py
+++ meeticket/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=b&yv3e5@!!8jdq5n$+41q^$z^=meynv=i8ypg1sovq0ll_z^v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
